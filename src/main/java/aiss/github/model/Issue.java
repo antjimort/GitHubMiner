01072eb29b64ff1790f5cdcc1c5d9e5709ed75e6
antjimort <antjimort@alum.us.es>
2023-05-06 21:50:30 +0200

    METER EN EL TOSTRING DE ISSUES upvotes y downvotes

---
+++ src/main/java/aiss/github/model/Issue.java
@@ -0,0 +1,274 @@
+
+package aiss.github.model;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "ref_id",
+    "title",
+    "body",
+    "user",
+    "labels",
+    "state",
+    "assignee",
+    "created_at",
+    "updated_at",
+    "closed_at",
+    "reactions"
+})
+@Generated("jsonschema2pojo")
+public class Issue {
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("ref_id")
+    private String refId;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("body")
+    private String body;
+    @JsonProperty("user")
+    private User user;
+    @JsonProperty("labels")
+    private List<Object> labels;
+    @JsonProperty("state")
+    private String state;
+    @JsonProperty("assignee")
+    private Object assignee;
+    @JsonProperty("created_at")
+    private String createdAt;
+    @JsonProperty("updated_at")
+    private String updatedAt;
+    @JsonProperty("closed_at")
+    private Object closedAt;
+    @JsonProperty("reactions")
+    private Reactions reactions;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("ref_id")
+    public String getRefId() {
+        return refId;
+    }
+
+    @JsonProperty("ref_id")
+    public void setRefId(String refId) {
+        this.refId = refId;
+    }
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("body")
+    public String getBody() {
+        return body;
+    }
+
+    @JsonProperty("body")
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @JsonProperty("user")
+    public User getUser() {
+        return user;
+    }
+
+    @JsonProperty("user")
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @JsonProperty("labels")
+    public List<Object> getLabels() {
+        return labels;
+    }
+
+    @JsonProperty("labels")
+    public void setLabels(List<Object> labels) {
+        this.labels = labels;
+    }
+
+    @JsonProperty("state")
+    public String getState() {
+        return state;
+    }
+
+    @JsonProperty("state")
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @JsonProperty("assignee")
+    public Object getAssignee() {
+        return assignee;
+    }
+
+    @JsonProperty("assignee")
+    public void setAssignee(Object assignee) {
+        this.assignee = assignee;
+    }
+
+    @JsonProperty("created_at")
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    @JsonProperty("created_at")
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @JsonProperty("updated_at")
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @JsonProperty("updated_at")
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @JsonProperty("closed_at")
+    public Object getClosedAt() {
+        return closedAt;
+    }
+
+    @JsonProperty("closed_at")
+    public void setClosedAt(Object closedAt) {
+        this.closedAt = closedAt;
+    }
+
+    @JsonProperty("reactions")
+    public Reactions getReactions() {
+        return reactions;
+    }
+
+    @JsonProperty("reactions")
+    public void setReactions(Reactions reactions) {
+        this.reactions = reactions;
+    }
+
+    @JsonProperty("upvotes")
+    public Integer getUpvotes() {
+        return reactions.getUpvotes();
+    }
+
+    @JsonProperty("upvotes")
+    public void setUpvotes(Integer upvotes) {
+        reactions.setUpvotes(upvotes);
+    }
+
+    @JsonProperty("downvotes")
+    public Integer getDownvotes() {
+        return reactions.getDownvotes();
+    }
+
+    @JsonProperty("downvotes")
+    public void setDownvotes(Integer downvotes) {
+        reactions.setDownvotes(downvotes);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Issue.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("refId");
+        sb.append('=');
+        sb.append(((this.refId == null)?"<null>":this.refId));
+        sb.append(',');
+        sb.append("title");
+        sb.append('=');
+        sb.append(((this.title == null)?"<null>":this.title));
+        sb.append(',');
+        sb.append("body");
+        sb.append('=');
+        sb.append(((this.body == null)?"<null>":this.body));
+        sb.append(',');
+        sb.append("user");
+        sb.append('=');
+        sb.append(((this.user == null)?"<null>":this.user));
+        sb.append(',');
+        sb.append("labels");
+        sb.append('=');
+        sb.append(((this.labels == null)?"<null>":this.labels));
+        sb.append(',');
+        sb.append("state");
+        sb.append('=');
+        sb.append(((this.state == null)?"<null>":this.state));
+        sb.append(',');
+        sb.append("assignee");
+        sb.append('=');
+        sb.append(((this.assignee == null)?"<null>":this.assignee));
+        sb.append(',');
+        sb.append("createdAt");
+        sb.append('=');
+        sb.append(((this.createdAt == null)?"<null>":this.createdAt));
+        sb.append(',');
+        sb.append("updatedAt");
+        sb.append('=');
+        sb.append(((this.updatedAt == null)?"<null>":this.updatedAt));
+        sb.append(',');
+        sb.append("closedAt");
+        sb.append('=');
+        sb.append(((this.closedAt == null)?"<null>":this.closedAt));
+        sb.append(',');
+        sb.append("reactions");
+        sb.append('=');
+        sb.append(((this.reactions == null)?"<null>":this.reactions));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
---
+++ src/main/java/aiss/github/model/User.java
@@ -0,0 +1,134 @@
+
+package aiss.github.model;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "login",
+    "id",
+    "name",
+    "avatar_url",
+    "html_url"
+})
+@Generated("jsonschema2pojo")
+public class User {
+
+    @JsonProperty("login")
+    private String login;
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("avatar_url")
+    private String avatarUrl;
+    @JsonProperty("html_url")
+    private String htmlUrl;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("login")
+    public String getLogin() {
+        return login;
+    }
+
+    @JsonProperty("login")
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("avatar_url")
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    @JsonProperty("avatar_url")
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    @JsonProperty("html_url")
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    @JsonProperty("html_url")
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(User.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("login");
+        sb.append('=');
+        sb.append(((this.login == null)?"<null>":this.login));
+        sb.append(',');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("name");
+        sb.append('=');
+        sb.append(((this.name == null)?"<null>":this.name));
+        sb.append(',');
+        sb.append("avatarUrl");
+        sb.append('=');
+        sb.append(((this.avatarUrl == null)?"<null>":this.avatarUrl));
+        sb.append(',');
+        sb.append("htmlUrl");
+        sb.append('=');
+        sb.append(((this.htmlUrl == null)?"<null>":this.htmlUrl));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
